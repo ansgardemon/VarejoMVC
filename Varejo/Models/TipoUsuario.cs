2b87e744188eadd2d271e0679e13f33f2e763074
MATHEUS BEZERRA DE SOUZA <matheus.bsouza22@senacsp.edu.br>
2025-09-22 20:14:29 -0300

    MODELS PESSOAS E RELACIONADOS

---
+++ Varejo/Models/TipoUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Varejo.Models
+{
+    public class TipoUsuario
+    {
+
+        [Key]
+        public int IdTipoUsuario { get; set; }
+
+
+
+        [Required]
+        [StringLength(20)]
+        public string DescricaoTipoUsuario { get; set; }
+
+        
+        
+        public virtual ICollection<Usuario>? Usuarios { get; set; }
+
+
+
+    }
+}
---
+++ Varejo/Models/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Varejo.Models
+{
+    public class Usuario
+    {
+
+        [Key]
+        public int IdUsuario { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string nomeUsuario { get; set; }
+
+        [StringLength(15)]
+        [Required]
+        public string Senha { get; set; }
+
+
+
+
+
+        //RELACIONAMENTO COM PESSOA
+        public int PessoaId { get; set; }
+
+        public virtual Pessoa? Pessoa { get; set; }
+
+
+        public int TipoUsuarioId { get; set; }
+
+        public virtual TipoUsuario? TipoUsuario { get; set; }
+
+
+    }
+}

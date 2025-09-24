0879f1af58907a7be95ccdecca35bf24fb44ea5a
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2025-09-24 19:28:11 -0300

    aa

---
+++ Varejo/Controllers/PessoaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Varejo.Interfaces;
+
+
+namespace Varejo.Controllers;
+
+public class PessoaController : Controller
+{
+    private readonly IPessoaRepository _pessoaRepository;
+
+    public PessoaController(IPessoaRepository pessoaRepository)
+    {
+        _pessoaRepository = pessoaRepository;
+    }
+
+    // == CRUD ==
+
+    //READ
+    [Authorize]
+    public async Task<IActionResult> Index()
+    {
+        var pessoas = await _pessoaRepository.GetAllAsync();
+        pessoas = pessoas.OrderBy(d => d.NomeRazao).ToList();
+
+        return View(pessoas);
+    }
+
+
+}
+
+

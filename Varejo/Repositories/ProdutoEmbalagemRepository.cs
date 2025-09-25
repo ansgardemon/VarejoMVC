e90455c879cfba1b4571d33672683659898c0802
daniel.afiengo <daniel.afiengo@SMP0711446W11-1>
2025-09-24 21:11:13 -0300

    Repositorio Produto Embalagem implementado.

--- Varejo/Repositories/ProdutoEmbalagemRepository.cs
+++ Varejo/Repositories/ProdutoEmbalagemRepository.cs
@@ -1,33 +1,49 @@
-﻿using Varejo.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using Varejo.Data;
+using Varejo.Interfaces;
 using Varejo.Models;
 
 namespace Varejo.Repositories
 {
     public class ProdutoEmbalagemRepository : IProdutoEmbalagemRepository
     {
-        public Task AddAsync(ProdutoEmbalagem produtoEmbalagem)
+        private readonly VarejoDbContext _context;
+
+        public ProdutoEmbalagemRepository(VarejoDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        public async Task AddAsync(ProdutoEmbalagem produtoEmbalagem)
+        {
+            await _context.AddAsync(produtoEmbalagem);
+            await _context.SaveChangesAsync();
         }
 
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var produtoEmbalagem = _context.ProdutosEmbalagem.Find(id);
+            if (produtoEmbalagem != null)
+            {
+                _context.ProdutosEmbalagem.Remove(produtoEmbalagem);
+                _context.SaveChanges();
+            }
         }
 
-        public Task<List<ProdutoEmbalagem>> GetAllAsync()
+        public async Task<List<ProdutoEmbalagem>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await _context.ProdutosEmbalagem.ToListAsync();
         }
 
-        public Task<ProdutoEmbalagem?> GetByIdAsync(int id)
+        public async Task<ProdutoEmbalagem?> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.ProdutosEmbalagem
+                                 .FirstOrDefaultAsync(e => e.IdProdutoEmbalagem == id);
         }
 
-        public Task UpdateAsync(ProdutoEmbalagem produtoEmbalagem)
+        public async Task UpdateAsync(ProdutoEmbalagem produtoEmbalagem)
         {
-            throw new NotImplementedException();
+            _context.ProdutosEmbalagem.Update(produtoEmbalagem);
+            await _context.SaveChangesAsync();
         }
     }
 }

1800cc45aaf0feda74f8e6aa3420e59d8146e77d
MATHEUS BEZERRA DE SOUZA <matheus.bsouza22@senacsp.edu.br>
2025-09-29 20:09:56 -0300

    SHULEPO LEPO

--- Varejo/Controllers/PessoaController.cs
+++ Varejo/Controllers/PessoaController.cs
@@ -12,7 +12,6 @@ namespace Varejo.Controllers
     public class PessoaController : Controller
     {
         private readonly IPessoaRepository _pessoaRepository;
-        private readonly IFamiliaRepository _familiaRepository;
 
 
         public PessoaController(IPessoaRepository pessoaRepository)
@@ -20,46 +19,122 @@ namespace Varejo.Controllers
             _pessoaRepository = pessoaRepository;
         }
 
-        // == CRUD ==
 
-        //CREATE
-        public async Task<IActionResult> Create() => View();
+        //CREATE READ
+
+        public async  Task<IActionResult> Create()
+        {
+            // Retorna o formulário em branco
+            var vm = new PessoaViewModel
+            {
+                Enderecos = new List<EnderecoViewModel> { new EnderecoViewModel() }
+            };
+
+            return View(vm);
+        }
+
+
+
+
+        // CREATE POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Pessoa pessoa)
+        public async Task<IActionResult> Create(PessoaViewModel pessoaVm)
         {
             if (ModelState.IsValid)
             {
+                var pessoa = new Pessoa
+                {
+                    NomeRazao = pessoaVm.NomeRazao,
+                    TratamentoFantasia = pessoaVm.TratamentoFantasia,
+                    CpfCnpj = pessoaVm.CpfCnpj,
+                    Ddd = pessoaVm.Ddd,
+                    Telefone = pessoaVm.Telefone,
+                    Email = pessoaVm.Email,
+                    EhJuridico = pessoaVm.EhJuridico,
+                    EhUsuario = pessoaVm.EhUsuario,
+                    EhCliente = pessoaVm.EhCliente,
+                    EhFornecedor = pessoaVm.EhFornecedor,
+                    Ativo = pessoaVm.Ativo,
+
+                    Enderecos = pessoaVm.Enderecos?.Select(e => new Endereco
+                    {
+                        Logradouro = e.Logradouro,
+                        Numero = e.Numero,
+                        Cep = e.Cep,
+                        Bairro = e.Bairro,
+                        Cidade = e.Cidade,
+                        Uf = e.Uf,
+                        Complemento = e.Complemento
+                    }).ToList()
+                };
+
                 await _pessoaRepository.AddAsync(pessoa);
                 return RedirectToAction(nameof(Index));
             }
-            return View(pessoa);
+
+            return View(pessoaVm);
         }
 
-        //READ
+
+        //READ INDEX
         //[Authorize]
+        //DEIXEI ENXUTO, POIS OS MAIORES DADOS VÃO PRO DETAILS
         public async Task<IActionResult> Index()
         {
             var pessoas = await _pessoaRepository.GetAllAsync();
-            var pessoasVm = pessoas.OrderBy(p => p.NomeRazao).Select(p => new PessoaViewModel
+
+            var pessoasVm = pessoas.Select(p => new PessoaViewModel
             {
                 IdPessoa = p.IdPessoa,
                 NomeRazao = p.NomeRazao,
-                TratamentoFantasia = p.TratamentoFantasia,
                 CpfCnpj = p.CpfCnpj,
-                Ddd = p.Ddd,
                 Telefone = p.Telefone,
                 Email = p.Email,
-                EhJuridico = p.EhJuridico,
-                EhUsuario = p.EhUsuario,
-                EhCliente = p.EhCliente,
-                EhFornecedor = p.EhFornecedor,
                 Ativo = p.Ativo
             }).ToList();
+
             return View(pessoasVm);
         }
 
- 
+
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var pessoa = await _pessoaRepository.GetByIdAsync(id);
+
+            if (pessoa == null) return NotFound();
+
+            var pessoaVm = new PessoaViewModel
+            {
+                IdPessoa = pessoa.IdPessoa,
+                NomeRazao = pessoa.NomeRazao,
+                TratamentoFantasia = pessoa.TratamentoFantasia,
+                CpfCnpj = pessoa.CpfCnpj,
+                Ddd = pessoa.Ddd,
+                Telefone = pessoa.Telefone,
+                Email = pessoa.Email,
+                EhJuridico = pessoa.EhJuridico,
+                EhUsuario = pessoa.EhUsuario,
+                EhCliente = pessoa.EhCliente,
+                EhFornecedor = pessoa.EhFornecedor,
+                Ativo = pessoa.Ativo,
+                Enderecos = pessoa.Enderecos?.Select(e => new EnderecoViewModel
+                {
+                    Logradouro = e.Logradouro,
+                    Numero = e.Numero,
+                    Cep = e.Cep,
+                    Bairro = e.Bairro,
+                    Cidade = e.Cidade,
+                    Uf = e.Uf,
+                    Complemento = e.Complemento
+                }).ToList()
+            };
+
+            return View(pessoaVm);
+        }
+
+
 
         //UPDATE
         // GET: Edit

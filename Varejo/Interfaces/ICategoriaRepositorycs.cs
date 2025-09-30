a5deca95495317477aa6695af6679f64bf852743
MATHEUS BEZERRA DE SOUZA <matheus.bsouza22@senacsp.edu.br>
2025-09-29 22:15:14 -0300

    Frango de boi

---
+++ Varejo/Interfaces/ICategoriaRepositorycs.cs
@@ -0,0 +1,13 @@
+﻿using Varejo.Models;
+
+namespace Varejo.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        Task<List<Categoria>> GetAllAsync();
+        Task<Categoria?> GetByIdAsync(int id);
+        Task AddAsync(Categoria categoria);
+        Task UpdateAsync(Categoria categoria);
+        Task DeleteAsync(int id);
+    }
+}

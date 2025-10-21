653dd7d13569469b4678532b7780f9f356c623ed
daniel.afiengo <daniel.afiengo@SMP0711446W11-1>
2025-10-21 20:36:45 -0300

    Segue API com Get do descricaoCategoria
    Controller da API contem apenas o GET

---
+++ VarejoAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Varejo.Interfaces;
+using Varejo.ViewModels;
+using VarejoAPI.DTO;
+
+namespace VarejoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaController(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCategoria()
+        {
+            var categorias = await _categoriaRepository.GetAllAsync();
+            var DTOlst = categorias.Select(c => new CategoriaOutputDTO
+            {
+                IdCategoria = c.IdCategoria,
+                DescricaoCategoria = c.DescricaoCategoria,
+            }).ToList();
+
+            return Ok(DTOlst);
+        }    
+
+
+    }
+}
--- VarejoAPI/DTO/CategoriaOutputDTO.cs
+++ VarejoAPI/DTO/CategoriaOutputDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace VarejoAPI.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VarejoAPI.DTO
 {
     public class CategoriaOutputDTO
     {
+
+        [Required(ErrorMessage ="Nome da Categoria é obrigatoria")]
+        public int IdCategoria { get; set; }
+
+
+        [Required(ErrorMessage = "Nome da descrição é obrigatoria")]
+        public string DescricaoCategoria { get; set; }
+
     }
 }

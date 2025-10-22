c2f7a3074ead835ff9b3cf0aeea7989145f18609
daniel.afiengo <daniel.afiengo@SMP0711446W11-1>
2025-10-21 21:47:27 -0300

    AAA

--- Varejo/wwwroot/js/landing.js
+++ Varejo/wwwroot/js/landing.js
@@ -1,3 +1,25 @@
 ﻿$(document).ready(function () {
-    const urlBase = "http://localhost:5018/api/Categoria";"
+    const urlBase = "http://localhost:5018/api/Categoria"; "
+
+    $ajax({
+        url: urlBase,
+        type: "GET",
+        contentType: "application/json",
+        success: function (data) {
+            let categorias = $('#categorias');
+            categorias.empty(); // Limpa o conteúdo existente
+            data.forEach(function (categoria) {
+                let categoriaItem = `
+                    <div class="categoria-item">
+                        <h3>${categoria.nome}</h3>
+                        <p>${categoria.descricao}</p>
+                    </div>
+                `;
+                categorias.append(categoriaItem);
+            });
+        },
+        error: function (error) {
+            console.error("Erro ao carregar categorias:", error);
+        }
+    })
 });

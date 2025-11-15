69380aea4cedb62b2125f095d87be24db2ec157d
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2025-11-14 22:08:10 -0300

    sajhvgdkajsdnijklsa

--- VarejoAPI/Interfaces/ICategoriaRepository.cs
+++ VarejoAPI/Interfaces/ICategoriaRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace VarejoAPI.Interfaces
+﻿using Varejo.Models;
+
+namespace VarejoAPI.Interfaces
 {
     public interface ICategoriaRepository
     {
+        //CREATE
+        Task AddAsync(Categoria categoria);
+
+        //UPDATE
+        Task UpdateAsync(Categoria categoria);
+
+        //DELETE
+        Task DeleteAsync(Categoria categoria);
     }
 }
--- VarejoAPI/Repositories/CategoriaRepository.cs
+++ VarejoAPI/Repositories/CategoriaRepository.cs
@@ -1,6 +1,40 @@
-﻿namespace VarejoAPI.Repositories
+﻿using Varejo.Models;
+using VarejoAPI.Interfaces;
+using Varejo.Data;
+
+namespace VarejoAPI.Repositories
 {
-    public class CategoriaRepository
+    public class CategoriaRepository : ICategoriaRepository
     {
+        private readonly VarejoDbContext _context;
+        public CategoriaRepository(VarejoDbContext context)
+        {
+            _context = context;
+        }
+
+        //CREATE
+        public async Task AddAsync(Categoria categoria)
+        {
+            await _context.Categorias.AddAsync(categoria);
+            await _context.SaveChangesAsync();
+        }
+
+        //UPDATE
+        public async Task UpdateAsync(Categoria categoria)
+        {
+            _context.Categorias.Update(categoria);
+            await _context.SaveChangesAsync();
+        }
+
+        //DELETE
+        public async Task DeleteAsync(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+            if (categoria != null)
+            {
+                _context.Categorias.Remove(categoria);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }

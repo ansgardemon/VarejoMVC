06763cdb5f7872c0751823be82f56064f0d74690
mathe <mathe@FULLBRINGER>
2026-03-16 23:36:12 -0300

    TipoMovimento

---
+++ VarejoAPI/DTO/TipoMovimentoInputDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace VarejoAPI.DTO
+{
+    public class TipoMovimentoInputDTO
+    {
+        public string DescricaoTipoMovimento { get; set; }
+        public int IdEspecieMovimento { get; set; }
+    }
+}

7b4ab5185942f21b34750eae9f24ce7fb9035083
Filipe-Alexandre <filipe.alexandre.360@gmail.com>
2026-03-23 22:28:54 -0300

    adicionando theme.js

---
+++ VarejoCLIENT/wwwroot/js/theme.js
@@ -0,0 +1,9 @@
+﻿window.toggleTheme = (isDark) => {
+    const root = document.documentElement;
+
+    if (isDark) {
+        root.setAttribute("data-theme", "dark");
+    } else {
+        root.removeAttribute("data-theme");
+    }
+};

5dc5b233f244be088d40a77fef20be2f31969dfe
Filipe-Alexandre <filipe.alexandre.360@gmail.com>
2026-03-23 22:26:37 -0300

    adicionando componentes e pages genericas de relatórios

---
+++ VarejoCLIENT/Models/UI/RelatorioColuna.cs
@@ -0,0 +1,9 @@
+﻿namespace Varejo.Models.UI
+{
+    public class RelatorioColuna<T>
+    {
+        public string Titulo { get; set; } = "";
+        public string Campo { get; set; } = "";
+        public Func<T, object> Selector { get; set; } = default!;
+    }
+}

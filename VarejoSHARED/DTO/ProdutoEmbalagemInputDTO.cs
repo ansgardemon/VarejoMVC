f35f590cc218c3786f548e0c989272c99f5a93cc
matheus.bsouza22 <matheus.bsouza22@SMP0711445W11-1>
2026-03-25 20:26:28 -0300

    AGORA VAI É TETRA

--- VarejoSHARED/DTO/ProdutoEmbalagemInputDTO.cs
+++ VarejoSHARED/DTO/ProdutoEmbalagemInputDTO.cs
@@ -1,6 +1,5 @@
 ﻿public class ProdutoEmbalagemInputDTO
 {
-    public int ProdutoId { get; set; }
     public decimal Preco { get; set; }
     public string Ean { get; set; }
     public int TipoEmbalagemId { get; set; }

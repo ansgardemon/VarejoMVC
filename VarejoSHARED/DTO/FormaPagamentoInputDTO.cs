08ba7747321f8dbd45fe86ea93b36796cca07028
GABRIEL RIBEIRO BENEVENUTO <gabriel.rbenevenuto@senacsp.edu.br>
2026-03-25 22:14:50 -0300

    Dtos
    Adiciona DTOs financeiros para VarejoSHARED
    
    Inclui oito novos arquivos DTO (InputDTO e OutputDTO) para as entidades FormaPagamento, PagamentoTitulo, PrazoPagamento e TituloFinanceiro, facilitando a transferência de dados no módulo financeiro. Observadas pequenas inconsistências na capitalização dos namespaces.

---
+++ VarejoSHARED/DTO/FormaPagamentoInputDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VarejoSHARED.DTO.Financeiro.FormaPagamento
+{
+    public class FormaPagamentoInputDTO
+    {
+        public string DescricaoFormaPagamento { get; set; }
+
+    }
+}
---
+++ VarejoSHARED/DTO/PagamentoTituloInputDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VarejoSHARED.DTO.Financeiro.PagamentoTitulo
+{
+    public class PagamentoTituloInputDTO
+    {
+        public int TituloFinanceiroId { get; set; }
+
+        public decimal ValorPago { get; set; }
+
+        public DateTime DataPagamento { get; set; }
+
+        public string? Observacao { get; set; }
+    }
+}

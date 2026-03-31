d21001f99806bb72a1a89372219b4a875af957b0
mathe <mathe@FULLBRINGER>
2026-03-31 18:34:53 -0300

    estoque minimo maximo

---
+++ Varejo/ViewModels/EstoqueConfigViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Varejo.ViewModels
+{
+    public class EstoqueConfigViewModel
+    {
+        public int Id { get; set; } // 0 para novo, > 0 para editar
+        public int ProdutoId { get; set; }
+        public string? NomeProduto { get; set; } // Apenas para exibição
+
+        public decimal EstoqueMinimo { get; set; }
+        public decimal EstoqueMaximo { get; set; }
+    }
+}

c775813ebd0068b1122e1265b84a79430f30f2f4
Filipe-Alexandre <filipe.alexandre.360@gmail.com>
2026-04-01 22:26:53 -0300

    migration historico de preço

---
+++ Varejo/Migrations/20260402012550_HistoricoPreco.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Varejo.Migrations
+{
+    /// <inheritdoc />
+    public partial class HistoricoPreco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HistoricosPreco",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProdutoId = table.Column<int>(type: "int", nullable: false),
+                    DataAlteracao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PrecoAntigo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoNovo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Usuario = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistoricosPreco", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HistoricosPreco_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "IdProduto",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HistoricosPreco_ProdutoId",
+                table: "HistoricosPreco",
+                column: "ProdutoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HistoricosPreco");
+        }
+    }
+}

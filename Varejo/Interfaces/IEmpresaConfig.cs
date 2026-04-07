692835ad8f5ad45280a436eb7bfcc4f79bd08f3c
matheus.bsouza22 <matheus.bsouza22@SMP0711445W11-1>
2026-04-06 21:11:14 -0300

    Recebimento

---
+++ Varejo/Interfaces/IEmpresaConfig.cs
@@ -0,0 +1,10 @@
+﻿using Varejo.Models;
+
+namespace Varejo.Interfaces
+{
+    public interface IEmpresaRepository
+    {
+        Task<ConfiguracaoEmpresa> GetConfiguracaoAsync();
+        Task<bool> SalvarConfiguracaoAsync(ConfiguracaoEmpresa config);
+    }
+}
---
+++ Varejo/Interfaces/IFornecedorConfigRepository.cs
@@ -0,0 +1,20 @@
+﻿using Varejo.Models;
+
+namespace Varejo.Interfaces
+{
+    public interface IFornecedorConfigRepository
+    {
+        // --- Gestão de Famílias do Fornecedor ---
+        Task<IEnumerable<Familia>> GetFamiliasPorFornecedorAsync(int pessoaId);
+        Task<bool> VincularFamiliaAsync(int pessoaId, int familiaId);
+        Task<bool> RemoverFamiliaAsync(int pessoaId, int familiaId);
+
+        // --- Gestão de Vínculo de Produtos (De-Para) ---
+        Task<IEnumerable<ProdutoFornecedorVinculo>> GetVinculosPorFornecedorAsync(int pessoaId);
+
+        // Busca se aquele código <cProd> do XML já existe para aquele fornecedor
+        Task<ProdutoFornecedorVinculo> GetVinculoPorCodigoExternoAsync(int pessoaId, string codigoExterno);
+
+        Task<bool> SalvarVinculoProdutoAsync(ProdutoFornecedorVinculo vinculo);
+    }
+}

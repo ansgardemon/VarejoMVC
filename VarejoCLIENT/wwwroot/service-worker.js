7131d660234a9b3949927d171260006cebd371dc
Filipe Alexandre <filipe.alexandre.360@gmail.com>
2026-04-11 23:32:52 -0300

    !!!

--- VarejoCLIENT/wwwroot/service-worker.js
+++ VarejoCLIENT/wwwroot/service-worker.js
@@ -1,1 +1,17 @@
 ﻿
+
+self.addEventListener('install', event => {
+    console.log('Service Worker: Instalando...');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker: Ativado e pronto para gerenciar o PWA.');
+});
+
+// Interceptador de requisições padrão (exigência para ser um PWA instalável)
+self.addEventListener('fetch', event => {
+    // Por enquanto, apenas deixa a requisição passar normalmente (Network first)
+    // Mais tarde, quando formos fazer cache offline, mexeremos aqui.
+    return;
+});
